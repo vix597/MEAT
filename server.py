15ad622b01a7030fc05c0e523e8c9bbea180f20b
Sean LaPlante <seanlaplante@Mac.local>
2014-06-21 16:31:18 -0400

    Added argparse to server.py - port option

--- server.py
+++ server.py
@@ -1,5 +1,15 @@
+import argparse
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     from meat import app, socketio
-    socketio.run(app)
+
+    parser = argparse.ArgumentParser(description="MEAT - Monitor Every Awesome Thing!")
+    parser.add_argument('-p','--port',help="Run the server on port (Default is 5000)",type=int)
+    args = parser.parse_args()
+
+    if args.port:
+        socketio.run(app,port=args.port)
+    else:
+        socketio.run(app)
+
+    
